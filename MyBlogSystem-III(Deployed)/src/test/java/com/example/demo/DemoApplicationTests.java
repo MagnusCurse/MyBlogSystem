fa2b5041338aad2f4cb046403d01e38804c8fe0e
liu-jinxii <arny1371621210@163.com>
2024-08-17 16:25:24 +0800

    Scheduled Publish

--- MyBlogSystem-III(Deployed)/src/test/java/com/example/demo/DemoApplicationTests.java
+++ MyBlogSystem-III(Deployed)/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -10,35 +10,4 @@ import java.time.Instant;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void MD5test() {
-		String p1 = DigestUtils.md5DigestAsHex("123".getBytes());
-		String p2 = DigestUtils.md5DigestAsHex("123".getBytes());
-		System.out.println(p1);
-		System.out.println(p2);
-	}
-
-	@Test
-	void DelayTimeDuration() {
-		// Current time point
-		Instant now = Instant.now();
-
-		// Set the delay time
-		Duration delay = Duration.ofDays(1)
-				.plusHours(3)
-				.plusMinutes(20)
-				.plusSeconds(12);
-
-		// Calculate the time point after the delay
-		Instant delayedTime = now.plus(delay);
-
-		// Print results
-		System.out.println("Current time: " + now);
-		System.out.println("Delay:" + delay);
-		System.out.println("Time after delay: " + delayedTime);
-	}
 }
